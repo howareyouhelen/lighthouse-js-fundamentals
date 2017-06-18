c1da3ee4d5e46692488fb5924ceaa3449982477e
Helen <howareyouhelen@hotmail.com>
2017-06-18 08:17:42 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,11 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+while (ingredients <= 7) {
+  console.log(ingredients);
+  ingredients++
+}
+
+console.log("ingredients:");
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i])
+}
